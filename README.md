9619990bbff27b87fa3091c5e11f346aa3b9d0a5
Patrick Wiseman <me@thephw.com>
2013-03-15 14:03:01 -0400

    Initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-secure_random_url_safe
-======================
-
-Super short rails service to get a secure random from ruby that is URL safe.  
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecureRandomUrlSafe::Application.config.secret_token = '7f430cdd102c2970a4926e2fbe1f1dc92ac3622e58dd05e94088bc584b514566ed8ff3b1101eeb5342db6c89c29957c56774eb96de323487f23a8009fc89926a'
